7732d4f3fee7afecd7a442ce7e627489399576aa
江桥 <1062241667@qq.com>
2019-05-29 15:23:30 +0800

    修改

--- tt-miniprogram/src/ttClient.php
+++ tt-miniprogram/src/ttClient.php
@@ -12,53 +12,68 @@ namespace TtMiniProgram;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 
-class ttClient{
+class ttClient
+{
     public $app_id;
     public $secret;
     public $uid;
-    
-    public function __construct($app_id,$uid,$secret)
+
+    public function __construct($app_id, $uid, $secret)
     {
-        $this->uid=$uid;
-        $this->app_id=$app_id;
-        $this->secret=$secret;
+        $this->uid = $uid;
+        $this->app_id = $app_id;
+        $this->secret = $secret;
     }
-    
-    public function getAccessToken(){
-        $grant_type='client_credential';
-        $url='https://developer.toutiao.com/api/apps/token';
-        $data=[
-            'app_id'=>$this->app_id,
-            'secret'=>$this->secret,
-            'grant_type'=>$grant_type
+
+    public function getAccessToken()
+    {
+        $grant_type = 'client_credential';
+        $url = 'https://developer.toutiao.com/api/apps/token';
+        $data = [
+            'app_id' => $this->app_id,
+            'secret' => $this->secret,
+            'grant_type' => $grant_type
         ];
-        $client=new Client();
-        $request=new Request('GET',$url.'?'.http_build_query($data),[],[]);
-        try{
-            $result=$client->send($request);
-            return $result;
-        }catch(\Exception $e){
+        $client = new Client();
+        $request = new Request('GET', $url . '?' . http_build_query($data), [], []);
+        try {
+            $result = $client->send($request, ['verify' => false]);
+            return $result->getBody()->getContents();
+        } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
     }
-    
-    public function jscode2session($code,$anonymous_code=''){
-        $url='https://developer.toutiao.com/api/apps/jscode2session';
-        $data=[
-            'app_id'=>$this->app_id,
-            'secret'=>$this->secret,
-            'code'=>$code,
+
+    public function jscode2session($code, $anonymous_code = '')
+    {
+        $url = 'https://developer.toutiao.com/api/apps/jscode2session';
+        $data = [
+            'app_id' => $this->app_id,
+            'secret' => $this->secret,
+            'code' => $code,
         ];
-        if($anonymous_code){
-            $data['anonymous_code']=$anonymous_code;
+        if ($anonymous_code) {
+            $data['anonymous_code'] = $anonymous_code;
         }
-        $client=new Client();
-        $request=new Request('GET',$url.'?'.http_build_query($data),[],'');
-        try{
-            $result=$client->send($request);
-            return $result;
-        }catch(\Exception $e){
+        $client = new Client();
+        $request = new Request('GET', $url . '?' . http_build_query($data), [], '');
+        try {
+            $result = $client->send($request, ['verify' => false]);
+            return $result->getBody()->getContents();
+        } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
+    }
+
+    public function checkSignature($rawData, $session_key, $signature)
+    {
+        return sha1($rawData . $session_key) === $signature;
+    }
+
+    public function getUserInfo($encryptedData, $session_key, $iv)
+    {
+        $key = base64_decode($session_key);
+        $iv = base64_decode($iv);
+        return openssl_decrypt(base64_decode($encryptedData), 'aes-128-cbc', $key, true, $iv);
     }
 }
--- tt-miniprogram/test/test.php
+++ tt-miniprogram/test/test.php
@@ -5,6 +5,7 @@
  * Date: 2019/4/8
  * Time: 11:38
  */
+
 namespace TtMiniProgram\test;
 include "../../vendor/autoload.php";
 
@@ -12,9 +13,14 @@ include "../../vendor/autoload.php";
 use TtMiniProgram\http;
 use TtMiniProgram\ttClient;
 
-$m=new ttClient(132321321,123213213,21321321321321);
-try{
+$m = new ttClient(132321321, 123213213, 21321321321321);
+try {
     var_dump($m->jscode2session(213213213213));
-}catch(\Exception $e){
+} catch (\Exception $e) {
     var_dump($e->getMessage());
 }
+
+$m = openssl_encrypt("江桥在上网", 'aes-128-cbc', "12321321321", true, "1234567891234567");
+echo $m."\r\n";
+
+echo openssl_decrypt($m, 'aes-128-cbc', "12321321321", true, "1234567891234567");
